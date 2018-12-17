02c514cd4598471610c2080f27c1c4ff8c4db83e
zorewy <624110778@qq.com>
2018-12-17 18:09:04 +0800

    init

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,8 +3,13 @@ const webpack = require('webpack')
 const config = require('../config')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const utils = require('./utils');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+function resolve (dir) {
+	return path.join(__dirname, '..', dir)
+}
 
 module.exports = {
 	context: path.resolve(__dirname, '../'),
@@ -15,7 +20,7 @@ module.exports = {
 	output: {
 		path: config.build.assetsRoot,
 		filename: '[name].js',
-		chunkFilename: '[name].js',
+		// chunkFilename: '[name].js',
 		publicPath: process.env.NODE_ENV === 'production'
 			? config.build.assetsPublicPath
 			: config.dev.assetsPublicPath
@@ -23,48 +28,76 @@ module.exports = {
 	resolve: {
 		extensions: ['.js', '.jsx', '.json', '.md'],
 		alias: {
-			'@': path.resolve('src'),
+			'@': resolve('src'),
 		}
 	},
-	optimization: {
-		splitChunks: {
-			chunks: 'initial', // 只对入口文件处理
-			cacheGroups: {
-				vendor: { // split `node_modules`目录下被打包的代码到 `page/vendor.js && .css` 没找到可打包文件的话，则没有。css需要依赖 `ExtractTextPlugin`
-					test: /node_modules\//,
-					name: 'page/vendor',
-					priority: 10,
-					enforce: true
-				},
-				commons: { // split `common`和`components`目录下被打包的代码到`page/commons.js && .css`
-					test: /common\/|components\//,
-					name: 'page/commons',
-					priority: 10,
-					enforce: true
+	module: {
+		rules: [
+			{
+				test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+				loader: 'url-loader',
+				options: {
+					limit: 10000,
+					name: utils.assetsPath('img/[name].[hash:7].[ext]')
 				}
-			}
-		},
-		runtimeChunk: {
-			name: 'page/manifest'
-		}
+			},
+			{
+				test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+				loader: 'url-loader',
+				options: {
+					limit: 10000,
+					name: utils.assetsPath('media/[name].[hash:7].[ext]')
+				}
+			},
+			{
+				test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+				loader: 'url-loader',
+				options: {
+					limit: 10000,
+					name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+				}
+			},
+		]
 	},
+	// optimization: {
+	// 	splitChunks: {
+	// 		chunks: 'initial', // 只对入口文件处理
+	// 		cacheGroups: {
+	// 			vendor: { // split `node_modules`目录下被打包的代码到 `page/vendor.js && .css` 没找到可打包文件的话，则没有。css需要依赖 `ExtractTextPlugin`
+	// 				test: /node_modules\//,
+	// 				name: 'js/vendor',
+	// 				priority: 10,
+	// 				enforce: true
+	// 			},
+	// 			commons: { // split `common`和`components`目录下被打包的代码到`page/commons.js && .css`
+	// 				test: /common\/|components\//,
+	// 				name: 'js/commons',
+	// 				priority: 10,
+	// 				enforce: true
+	// 			}
+	// 		}
+	// 	},
+	// 	runtimeChunk: {
+	// 		name: 'page/manifest'
+	// 	}
+	// },
 	plugins: [
-		new CleanWebpackPlugin(['dist']),
-		new webpack.ProvidePlugin({
-			React:'react',
-			ReactDOM:'react-dom',
-			Component:['react','Component']
-		}),
-		new ExtractTextPlugin({
-			filename: '[name].css',
-			ignoreOrder: true
-		}),
+		new CleanWebpackPlugin(['../dist']),
+		// new webpack.ProvidePlugin({
+		// 	React:'react',
+		// 	ReactDOM:'react-dom',
+		// 	Component:['react','Component']
+		// }),
+		// new ExtractTextPlugin({
+		// 	filename: '[name].css',
+		// 	ignoreOrder: true
+		// }),
 		new HtmlWebpackPlugin(),
-		new ImageminPlugin({
-			disable: process.env.NODE_ENV !== 'production', // 开发时不启用
-			pngquant: { // 图片质量
-				quality: '95-100'
-			}
-		})
+		// new ImageminPlugin({
+		// 	disable: process.env.NODE_ENV !== 'production', // 开发时不启用
+		// 	pngquant: { // 图片质量
+		// 		quality: '95-100'
+		// 	}
+		// })
 	]
 }
