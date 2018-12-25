ea65eebfcc996b44edcb6177e7113abd448b8b5c
zorewy <624110778@qq.com>
2018-12-25 17:41:02 +0800

    init

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,7 +2,13 @@ import '@/assets/scss/index.scss'
 import React, { Component } from 'react'
 
 class Index extends Component {
+	constructor(props) {
+		super(props)
+		console.log(props)
+	}
 	render(){
+		const {props} = this
+
 		return (
 			<div className="test">111</div>
 		)
